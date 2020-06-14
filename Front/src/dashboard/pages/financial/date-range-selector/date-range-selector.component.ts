a761ab473fa60705828333eb1c18b6763b857d75
southerndind <testakina@gmail.com>
2020-06-14 11:28:18 +0900

    moment.addの引数の順番入れ替え

--- Front/src/dashboard/pages/financial/date-range-selector/date-range-selector.component.ts
+++ Front/src/dashboard/pages/financial/date-range-selector/date-range-selector.component.ts
@@ -11,11 +11,11 @@ export class DateRangeSelectorComponent extends DashboardParentComponent {
   /** 日付選択便利ボタン */
   public dateRanges = {
     "今月": [moment().startOf("month").toDate(), moment().toDate()],
-    "先月": [moment().add("months", -1).startOf("month").toDate(), moment().add("months", -1).endOf("month").toDate()],
-    "先々月": [moment().add("months", -2).startOf("month").toDate(), moment().add("months", -2).endOf("month").toDate()],
+    "先月": [moment().add(-1, "months").startOf("month").toDate(), moment().add(-1, "months").endOf("month").toDate()],
+    "先々月": [moment().add(-2, "months").startOf("month").toDate(), moment().add(-2, "months").endOf("month").toDate()],
     "今年": [moment().startOf("year").toDate(), moment().toDate()],
-    "去年": [moment().add("years", -1).startOf("year").toDate(), moment().add("years", -1).endOf("year").toDate()],
-    "一昨年": [moment().add("years", -2).startOf("year").toDate(), moment().add("years", -2).endOf("year").toDate()],
+    "去年": [moment().add(-1, "years").startOf("year").toDate(), moment().add(-1, "years").endOf("year").toDate()],
+    "一昨年": [moment().add(-2, "years").startOf("year").toDate(), moment().add(-2, "years").endOf("year").toDate()],
   };
 
   /**
