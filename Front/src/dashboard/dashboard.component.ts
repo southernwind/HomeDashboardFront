b01d3ae82ec97d9894cc8c0a27413cad0f107a65
southerndind <testakina@gmail.com>
2020-07-05 17:16:16 +0900

    タイトル/言語変更

--- Front/src/dashboard/dashboard.component.ts
+++ Front/src/dashboard/dashboard.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { DashboardParentComponent } from './components/parent/dashboard-parent.component';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   templateUrl: "./dashboard.component.html",
   styleUrls: ["./dashboard-component.scss"],
+  providers: [Title]
 })
-export class DashboardComponent extends DashboardParentComponent implements OnInit {
-  ngOnInit(): void { }
+export class DashboardComponent extends DashboardParentComponent {
+  constructor(title: Title) {
+    super();
+    title.setTitle("Home Dashboard");
+  }
 }
