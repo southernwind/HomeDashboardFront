02322ccc70a06b38057f43f46e897434f71831cf
southerndind <testakina@gmail.com>
2020-11-23 13:55:21 +0900

    zabbixをリンクに追加

--- src/dashboard/pages/links/links.component.ts
+++ src/dashboard/pages/links/links.component.ts
@@ -6,6 +6,7 @@ import { DashboardParentComponent } from 'src/dashboard/components/parent/dashbo
 })
 export class LinksComponent extends DashboardParentComponent {
   public links: { label: string, url: string }[] = [
-    { label: "jenkins", url: "//jenkins.home-server.localnet/" }
+    { label: "jenkins", url: "//jenkins.home-server.localnet/" },
+    { label: "ZABBIX", url: "//zabbix.home-server.localnet/" }
   ];
 }
