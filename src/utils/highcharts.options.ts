81bdebe147283fbf0585d03ce36bc443d3ec2873
southernwind <testakina@gmail.com>
2024-02-18 10:46:29 +0900

    chartの灰色の色を薄く

--- src/utils/highcharts.options.ts
+++ src/utils/highcharts.options.ts
@@ -1,5 +1,5 @@
 import * as Highcharts from 'highcharts';
-const defaultHighChartsColors: string[] = ["#7cb5ec", "#434348", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"];
+const defaultHighChartsColors: string[] = ["#7cb5ec", "#636368", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"];
 
 export const highChartsColors = defaultHighChartsColors;
 export function getHighChartsColor(num: number): string {
